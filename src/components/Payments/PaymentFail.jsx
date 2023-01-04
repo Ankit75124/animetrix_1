c0fa6ea2012e4d3e335ec5da95ca861ee911b172
devil07625 <antivirus076@gmail.com>
2023-01-05 00:21:04 +0530

    Payment v1-05/01 00:20 commit

--- src/components/Payments/PaymentFail.jsx
+++ src/components/Payments/PaymentFail.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button, Container, Heading, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const PaymentFail = () => {
   return (
-    <div>PaymentFail</div>
-  )
-}
+    <Container h="90vh" p="16">
+      <VStack justifyContent={'center'} h="full" spacing={'4'}>
+        <RiErrorWarningFill size={'5rem'} />
+        <Heading> Payment Failed </Heading>
 
-export default PaymentFail
+        <Link to="/subscribe">
+          <Button variant="ghost">Try Again</Button>
+        </Link>
+      </VStack>
+    </Container>
+  );
+};
+
+export default PaymentFail;
--- src/components/Payments/PaymentSuccess.jsx
+++ src/components/Payments/PaymentSuccess.jsx
@@ -1,10 +1,60 @@
-import { Container } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { RiCheckboxCircleFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const PaymentSuccess = () => {
   return (
-    <Container>
-      
+    <Container h="90vh" p="16">
+      <Heading
+        my="8"
+        textAlign={'center'}
+      > You have pro pack</Heading>
+
+      <VStack 
+      boxShadow={"lg"}
+      alignItems="center"
+      borderRadius={'lg'}
+      pb="16"
+      > 
+
+      <Box 
+      w="full" bg={"green"} p="4" 
+      css={{
+        borderRadius: '8px 8px 0 0',
+      }}
+      >
+        <Text color={"white"}>
+          Payment Success
+        </Text>
+      </Box>
+
+      <Box p="4">
+        <VStack textAlign={'center'} px="8" mt="4" spacing="8">
+
+        <Text>
+          Congratulation you have pro pack now. You can watch all animes.
+        </Text>
+
+        <Heading size="4xl">
+          <RiCheckboxCircleFill />
+        </Heading>
+
+        </VStack>
+
+      </Box>
+
+      <Link to="/profile">
+      <Button variant="ghost">
+        Go to profile
+      </Button>
+      </Link>
+
+      <Heading size={"xs"}>
+        Reference: bjhasjdjsahdasjhdjaskdha
+      </Heading>
+
+      </VStack>
     </Container>
   )
 }
